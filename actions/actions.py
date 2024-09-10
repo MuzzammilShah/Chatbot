ab97f5fff142990100c3ac78ae3d69ba0310ef36
MuzzammilShah <muhammed.muzzammilshah@gmail.com>
2024-09-10 23:23:28 +0530

    First commit

---
+++ actions/actions.py
@@ -0,0 +1,20 @@
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "action_hello_world"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Hi, I am Muhammed!")
+        dispatcher.utter_message(text="It is really nice to meet you :)")
+        dispatcher.utter_message(text="You can ask me anything you would like to know about me!")
+
+        return []
