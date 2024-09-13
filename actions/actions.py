59a00a353fbbb7c75634fdb8378f3e436f05b04c
MuzzammilShah <muhammed.muzzammilshah@gmail.com>
2024-09-13 23:38:11 +0530

    Pilot Training data

--- actions/actions.py
+++ actions/actions.py
@@ -13,9 +13,13 @@ class ActionHelloWorld(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        dispatcher.utter_message(text="Hi, I am Muhammed!")
-        dispatcher.utter_message(text="It is really nice to meet you :)")
-        dispatcher.utter_message(text="You can ask me anything you would like to know about me!")
+        dispatcher.utter_message(text="Lovely, let's have a chat then!")
+        dispatcher.utter_message(text="You can ask anything you would like to know about me!")
+
+        data= [ { "title":"Tell me about yourself", "payload":"/ask_about_me" }, { "title":"View Resume", "payload":"/ask_resume" }, { "title":"Contact Me", "payload":"/ask_contact" } ]
+        message={"payload":"quickReplies","data":data}
+
+        dispatcher.utter_message(text="Or not sure what to ask? How about these:", json_message=message)
 
         # buttons = [
         #     {"title": "", "payload": "/select_sub_one{{\"eSubOne\":\"SubOne\"}}"},
